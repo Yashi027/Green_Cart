363def996c28bb5f979066b38a58fc5b1a1fa0f7
Yashi Bansal <bansalyashi163@gmail.com>
2025-10-28 22:16:04 +0530

    category and best seller section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,59 @@
-import { useContext, useState, createContext } from "react";
+import { useContext, useState, createContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets.js";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false)
+  const [product, setProduct] = useState([])
+
+  const [cartItems , setCartItems] = useState({})
+
+  const fetchProducts = async() => {
+     setProduct(dummyProducts)
+  }
+
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems)
+    if(cartData[itemId]){
+      cartData[itemId] += 1;
+    }else{
+      cartData[itemId] =1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to Cart")
+  }
+
+  const updateCartItem = (itemId,quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData)
+    toast.success("Cart Updated")
+  }
+
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if(cartData[itemId])
+    {
+      cartData[itemId] -= 1;
+      if(cartData[itemId] === 0)
+        delete cartData[itemId];
+    }
+    toast.success("Removed from cart");
+    setCartItems(cartData)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  },[])
 
   const value = {
     user,
@@ -16,7 +62,13 @@ export const AppContextProvider = ({ children }) => {
     setIsSeller,
     navigate,
     showUserLogin,
-    setShowUserLogin
+    setShowUserLogin,
+    product,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems
   };
 
   return (
