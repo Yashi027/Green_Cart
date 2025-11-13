c2573b80e772dab18b390022507725ffbe97444a
Yashi Bansal <bansalyashi163@gmail.com>
2025-11-13 22:20:24 +0530

    product_list-orders

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { assets } from '../../assets/assets';
+import { assets, categories } from '../../assets/assets.js';
 
 const AddProduct = () => {
     const [files,setFiles] = useState([]);
@@ -42,8 +42,8 @@ const AddProduct = () => {
                     <label className="text-base font-medium" htmlFor="category">Category</label>
                     <select onChange={(e) => setCategory(e.target.value)} value={category} id="category" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40">
                         <option value="">Select Category</option>
-                        {[{ name: 'Electronics' }, { name: 'Clothing' }, { name: 'Accessories' }].map((item, index) => (
-                            <option key={index} value={item.name}>{item.name}</option>
+                        {categories.map((item,index) => (
+                            <option key={index} value={item.path}>{item.path}</option>
                         ))}
                     </select>
                 </div>
@@ -57,7 +57,7 @@ const AddProduct = () => {
                         <input onChange={(e) => setofferPrice(e.target.value)} value={offerPrice} id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                 </div>
-                <button className="px-8 py-2.5 bg-primary text-white font-medium rounded">ADD</button>
+                <button className="px-8 py-2.5 bg-primary text-white font-medium rounded cursor-pointer">ADD</button>
             </form>
         </div>
   );
