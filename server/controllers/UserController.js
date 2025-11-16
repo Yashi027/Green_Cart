9b20af0097087e6d015f7854e7276f376e28f1ed
Yashi Bansal <bansalyashi163@gmail.com>
2025-11-16 17:47:49 +0530

    authentication

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -59,6 +59,32 @@ export const login = async (req,res) => {
 
     } catch (error) {
         console.log(error.message)
-        res.status(500).json({success:false,message: error.message})
+        return res.status(500).json({success:false,message: error.message})
+    }
+}
+
+export const isAuth = async(req,res) => {
+    try {
+        const userId = req.userId;
+        const user = await User.findById(userId).select("-password")
+        return res.status(200).json({success:true, user})
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({success:false,message: error.message})
+    }
+}
+
+export const logout = async (req,res) => {
+    try {
+      res.clearCookie('token',{
+            httpOnly:true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        }) 
+         return res.status(200).json({success:true, message:"Logged out"})
+ 
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({success:false,message: error.message})
     }
 }
