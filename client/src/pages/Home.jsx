82dd3650fb10b4e9c13fa55886c95e09702e7ce6
Yashi Bansal <bansalyashi163@gmail.com>
2025-12-19 19:49:18 +0530

    debugging

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import MainBanner from '../components/MainBanner';
 import Categories from '../components/Categories';
 import BestSeller from '../components/BestSeller';
 import BottomBanner from '../components/BottomBanner';
-import Newsletter from '../components/newsletter';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
   return (
--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useAppContext } from '../context/AppContext'
-import { dummyOrders } from '../assets/assets'
 
 const MyOrders = () => {
     const [myorders, setMyOrders] = useState([])
