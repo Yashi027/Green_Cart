3e7d88bc48fc444d1159a630f0b42a6d39105aa0
Yashi Bansal <bansalyashi163@gmail.com>
2025-12-19 21:20:49 +0530

    server respondence

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -19,8 +19,8 @@ export const register = async (req,res) => {
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7*24*60*60*1000
         })
 
@@ -50,8 +50,8 @@ export const login = async (req,res) => {
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7*24*60*60*1000
         })
 
@@ -78,8 +78,8 @@ export const logout = async (req,res) => {
     try {
       res.clearCookie('token',{
             httpOnly:true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            secure: true,
+            sameSite: 'none'
         }) 
          return res.status(200).json({success:true, message:"Logged out"})
  
